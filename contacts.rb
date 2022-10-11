9af139613a734a94f0ffd8767def0d6677b0170e
Cindy Qu <cindyqu42@gmail.com>
2022-10-11 15:05:31 -0700

    commit tree c3f379150f18f3c4a315bdbc33874aaf9aa0c89f

--- contacts.rb
+++ contacts.rb
@@ -12,14 +12,21 @@ def contacts
     "Freddie Mercury" => {
       name: "Freddie",
       email: "freddie@mercury.com",
-      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+      favorite_ice_cream_flavors: ["cookie dough", "mint chip"]
     }
   }
 end
 
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddie Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
-
 # print the output to the terminal for inspection
 pp remove_strawberry(contacts)
